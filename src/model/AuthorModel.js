ea739c72474667de1675f2adc04ae542273cbb22
Deepak <deepakvijayan31@gmail.com>
2022-05-22 23:53:52 +0530

    make it better

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/Library');
-// const Schema = mongoose.Schema;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:root@cluster0.uoaf66f.mongodb.net/library?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect('mongodb://localhost:27017/Library');
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
