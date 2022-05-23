f0885bfcb5a79ca807758d8f5b685bf03a65df19
Deepakvijayan7 <102506209+Deepakvijayan7@users.noreply.github.com>
2022-05-23 16:21:03 +0530

    Update BookModel.js

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/Library');
+// const Schema = mongoose.Schema;
+const MONGODB_URI='mongodb+srv://admin:root@cluster0.uoaf66f.mongodb.net/Library?retryWrites=true&w=majority';
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/Library');
 const Schema = mongoose.Schema;
 
 
